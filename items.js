b4ea70c8c0b3d51c2b1f933030617f7e606f914a
ChaitanyaAnnamreddy <120272918+ChaitanyaAnnamreddy@users.noreply.github.com>
2024-02-25 16:05:48 +0000

    Add files via upload

---
+++ items.js
@@ -0,0 +1,26 @@
+var items = [
+    {
+        name:'chicken manchuria',
+        price:240,
+    },
+    {
+        name:'veg manchuria',
+        price:220,
+    },
+    {
+        name:'veg biryani',
+        price:230,
+    },
+    {
+        name:'chicken drumsticks',
+        price:260,
+    },
+    {
+        name:'chicken lollipop',
+        price:210,
+    },
+    {
+        name:'paneer tikka',
+        price:235,
+    }
+]
---
+++ tables.js
@@ -0,0 +1,26 @@
+var alltables = [
+    {
+        tableId:'t1',
+        isOccupied:false,
+        numberofChairs:4,
+        orderedItems:[]
+    },
+    {
+        tableId:'t2',
+        isOccupied:true,
+        numberofChairs:6,
+        orderedItems:[]
+    },
+    {
+        tableId:'t3',
+        isOccupied:false,
+        numberofChairs:2,
+        orderedItems:[]
+    },
+    {
+        tableId:'t4',
+        isOccupied:false,
+        numberofChairs:8,
+        orderedItems:[]
+    }
+]
